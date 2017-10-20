dcc5cf743d10b1bb4495169e20b77f78a68584ca
Marcus Rands <mrands@hotmail.co.uk>
2017-10-20 13:09:24 +0100

    Created  partial view for the create/update form

--- spec/features/add_restaurant_spec.rb
+++ spec/features/add_restaurant_spec.rb
@@ -1,3 +1,11 @@
+feature 'new restaurant' do
+  scenario 'should be able to view new restaurant page' do
+    visit 'restaurants/new'
+    expect(page).to have_content ("New Restaurant")
+  end
+end
+
+
 feature 'User creates a new restaurant listing' do
     scenario 'They enter a restaurant name and description' do
       visit new_restaurant_path
